d5cee7cdab770a06080cc6bbe1fd8387b10baf58
George Kouros <george.kouros.ece@gmail.com>
2019-03-04 15:15:16 +0100

    Fix cam_info_pub file loading

--- cam_info_pub/src/cam_info_pub.cpp
+++ cam_info_pub/src/cam_info_pub.cpp
@@ -7,23 +7,25 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "cam_info_publisher_node");
   ros::NodeHandle nh;
+  ros::NodeHandle pnh("~");
+  ros::Publisher cam_info_pub = nh.advertise<sensor_msgs::CameraInfo>("camera_info", 1);
 
-  std::string camera_name;
-  if (!nh.getParam("camera_name", camera_name))
+  std::string cam_name;
+  if (!pnh.getParam("cam_name", cam_name))
   {
-    ROS_ERROR("camera_name is not set in the parameter server");
+    ROS_ERROR("cam_name is not set in the parameter server");
     exit(EXIT_FAILURE);
   }
-  ros::Publisher cam_info_pub = nh.advertise<sensor_msgs::CameraInfo>("camera_info", 1);
   
   std::string cam_info_path;
-  if (!nh.getParam("cam_info_path", cam_info_path))
+  if (!pnh.getParam("cam_info_path", cam_info_path))
   {
     ROS_ERROR("cam_info_path is not set in the parameter server");
     exit(EXIT_FAILURE);
   }
 
-  camera_info_manager::CameraInfoManager cam_info_mngr(nh, camera_name, "file:///"+cam_info_path);
+  std::string path = "file://${ROS_HOME}/" + cam_info_path;
+  camera_info_manager::CameraInfoManager cam_info_mngr(nh, cam_name, path);
 
   sensor_msgs::CameraInfo cam_info;
   cam_info = cam_info_mngr.getCameraInfo();
