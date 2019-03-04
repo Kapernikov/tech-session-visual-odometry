ef64bf0e362d129ba4b885b98c512c509e206232
George Kouros <george.kouros.ece@gmail.com>
2019-03-04 14:28:05 +0100

    Add node that publishes camera info from yaml file

--- cam_info_pub/src/cam_info_pub.cpp
+++ cam_info_pub/src/cam_info_pub.cpp
@@ -1,39 +1,40 @@
-//contains cameraInfo for snapcam.
-//MUST RENAME NODE:  snap_cam_highres_publisher/image (etc) to image_raw
-//MUST SET CAMFILE:  rosparam set camfile snapcam1 (etc)
 #include "ros/ros.h"
 #include "sensor_msgs/CameraInfo.h"
-#include "sensor_msgs/CompressedImage.h"
-#include "sensor_msgs/Image.h"
 #include "camera_info_manager/camera_info_manager.h"
 using namespace std;
 
-/**Code adapted from
-http://answers.ros.org/question/59725/publishing-to-a-topic-via-subscriber-callback-function/
-  More efficient code to modify image before republishing at
-http://answers.ros.org/question/53234/processing-an-image-outside-the-callback-function/
-*/
-
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "publishCameraInfo");
-  	ros::NodeHandle nh_; 
-  	ros::Publisher cam_info_pub_ = nh_.advertise<sensor_msgs::CameraInfo>("camera_info", 1);
-    
-	const std::string camnameConst="snapcam1";
-    const std::string camurlRead="file:///home/haumin/.ros/camera_info/snapcam1.yaml"; 
-    camera_info_manager::CameraInfoManager caminfo(nh_, camnameConst,camurlRead);
-    
-	sensor_msgs::CameraInfo cam_info_;
-    cam_info_ = caminfo.getCameraInfo();
+  ros::init(argc, argv, "cam_info_publisher_node");
+  ros::NodeHandle nh;
 
-    // Publish via image_transport
-    while(ros::ok()){
-		cam_info_pub_.publish(cam_info_);
-  		ros::spinOnce();
-	}
+  std::string camera_name;
+  if (!nh.getParam("camera_name", camera_name))
+  {
+    ROS_ERROR("camera_name is not set in the parameter server");
+    exit(EXIT_FAILURE);
+  }
+  ros::Publisher cam_info_pub = nh.advertise<sensor_msgs::CameraInfo>("camera_info", 1);
+  
+  std::string cam_info_path;
+  if (!nh.getParam("cam_info_path", cam_info_path))
+  {
+    ROS_ERROR("cam_info_path is not set in the parameter server");
+    exit(EXIT_FAILURE);
+  }
 
-  	return 0;
+  camera_info_manager::CameraInfoManager cam_info_mngr(nh, camera_name, "file:///"+cam_info_path);
+
+  sensor_msgs::CameraInfo cam_info;
+  cam_info = cam_info_mngr.getCameraInfo();
+
+  while(ros::ok())
+  {
+    cam_info_pub.publish(cam_info);
+    ros::spinOnce();
+  }
+
+  return 0;
 }
 
 
